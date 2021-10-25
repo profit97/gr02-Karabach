97c6faa6dccc2f28c2ae8b0bbd569032a4fa762f
profit97 <56477072+profit97@users.noreply.github.com>
2021-10-25 17:30:56 +0300

    Add files via upload

---
+++ exam17-a (2).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+    char s[256];
+    fgets(s, 256, stdin);
+
+    char *p = strchr(s, '\n');
+    if(p != NULL) p[0] = '\0';
+
+    size_t n = strlen(s);
+
+    printf("len: %ld\n", n);
+
+    int m = 0;
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", s[i]);
+        if (s[i] == ' ') m++;
+    }
+
+    if (n > 0) m++;
+    printf("words: %d\n", m);
+
+    return 0;
+}
